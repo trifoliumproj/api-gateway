3f65e4bca5de676f61684f1a7122857efb162317
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-20 18:51:20 +0700

    feat(nh): use bindNodeCallback()

--- src/nh/nh.service.ts
+++ src/nh/nh.service.ts
@@ -5,6 +5,8 @@ import {
   GalleryResponse,
 } from '../../api/generated/nhentai/nhentai_pb';
 import { ClientGrpc } from '@nestjs/microservices';
+import { Observable, bindNodeCallback } from 'rxjs';
+import { requestCallback } from 'grpc';
 
 interface APIClientProxy extends APIClient {}
 
@@ -18,14 +20,11 @@ export class NhService implements OnModuleInit {
     this.apiService = this.client.getService<APIClientProxy>('API');
   }
 
-  gallery(id: number): Promise<GalleryResponse> {
+  gallery(id: number): Observable<GalleryResponse> {
     let request = new GalleryRequest();
     request.setId(id);
-    return new Promise((resolve, reject) =>
-      this.apiService.gallery(request, (error, value) => {
-        if (error) return reject(error);
-        return resolve(value);
-      }),
-    );
+    return bindNodeCallback((callback: requestCallback<GalleryResponse>) =>
+      this.apiService.gallery(request, callback),
+    )();
   }
 }
