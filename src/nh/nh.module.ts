6097e5a0bb8d42bfca8f47733e26034f2a10e8b2
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-24 12:40:21 +0700

    fix(nh): NHENTAI_PACKAGE_NAME

--- src/nh/nh.module.ts
+++ src/nh/nh.module.ts
@@ -3,15 +3,16 @@ import { Transport, ClientsModule } from '@nestjs/microservices';
 import { NhService } from './nh.service';
 import { NhController } from './nh.controller';
 import { join } from 'path';
+import { NHENTAI_PACKAGE_NAME } from '../generated/api/nhentai';
 
 @Module({
   imports: [
     ClientsModule.register([
       {
-        name: '@trifoliumproj/nh',
+        name: NHENTAI_PACKAGE_NAME,
         transport: Transport.GRPC,
         options: {
-          package: 'nhentai',
+          package: NHENTAI_PACKAGE_NAME,
           protoPath: join(__dirname, '../../api/nhentai.proto'),
         },
       },
