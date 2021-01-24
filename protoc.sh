9fd80d1fd0a85543e213684d7dd34461f565844d
FleeingSunlight <gh@fleeingsunlight.dev>
2021-01-24 12:21:45 +0700

    chore(protoc.sh): update

--- protoc.sh
+++ protoc.sh
@@ -1,4 +1,4 @@
 cd $(dirname $0)
 mkdir -p ./src/generated
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/generated ./api/nhentai.proto
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/generated ./api/konachan.proto
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/generated --ts_proto_opt=nestJs=true ./api/nhentai.proto
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/generated --ts_proto_opt=nestJs=true ./api/konachan.proto
